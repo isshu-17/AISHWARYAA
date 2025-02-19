31bcb3f065e54f88a7ded61d19c84113e5d43bdd
isshu-17 <aishwarya.a@s.amity.edu>
2025-02-19 23:57:48 +0530

    print factorial

--- factorial.c
+++ factorial.c
@@ -3,3 +3,11 @@ int main(){
     int num;
     int fact =1,i;
     scanf("%d",&num);
+    for(i=1;i<=num;i++)
+       {
+	    fact=fact*i;
+       }
+      {
+	   printf("the factorial of the given number%d",fact);
+      }
+}
